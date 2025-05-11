c38f632636532246803f13f1588d9116862f14f1
tegarrramadhaaan <anakrama2000@gmail.com>
2025-05-11 23:33:35 +0700

    1

---
+++ popup.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    setTimeout(function() {
+      alert("Promo Diskon 20%! Daftar sekarang untuk mendapatkan penawaran spesial.");
+    }, 3000); // Muncul setelah 3 detik
+  };
+  
